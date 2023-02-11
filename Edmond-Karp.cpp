6252448bbf83c234bea6bd219d20c61c5cd20fe2
gumi-bd <104053727+gumi-bd@users.noreply.github.com>
2023-02-11 13:58:44 +0530

    added Edmond Karp algorithm

---
+++ Edmond-Karp.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+class maxFlow{
+    private:
+        int n;
+        vector<vector<pair<int,int>>> adj;
+        int s;
+        int t;
+        int maxflow = 0;
+    public:
+        maxFlow(int no, vector<vector<pair<int,int>>> edges, int start, int dest){
+            adj = edges;
+            n = no;
+            s = start;
+            t = dest;
+        }
+        vector<int> shortestPath();
+        void push(vector<int> path);
+        int getCapacity(int a, int b);
+        void changeEdge(int a, int b, int w);
+        int solve();
+};
+
+vector<int> maxFlow :: shortestPath(){
+    queue<int> BFS;
+    vector<int> visited(n, 0);
+    BFS.push(s);
+    visited[s] = 1;
+    while(!BFS.empty()){
+        int curr = BFS.front();
+        BFS.pop();
+        for(auto it : adj[curr]){
+            int k = it.first;
+            if(visited[k] == 0){
+                visited[k] = curr;
+                BFS.push(k);
+            }
+        }
+    }
+    int it = t;
+    vector<int> path;
+    if(visited[t] == 0) return path;
+    while(it != s){
+        path.push_back(it);
+        it = visited[it];
+    }
+    path.push_back(s);
+    reverse(path.begin(), path.end());
+    return path;
+}
+
+int maxFlow :: getCapacity(int a, int b){
+    for(auto it : adj[a]){
+        if(it.first == b) return it.second;
+    }
+}
+
+void maxFlow :: push(vector<int> path){
+    int capacity = INT_MAX;
+    for(int i = 0; i < path.size() - 1; i++){
+        capacity = min(capacity, getCapacity(path[i], path[i + 1]));
+    }
+    maxflow += capacity;
+    for(int i = 0; i < path.size() - 1; i++){
+        changeEdge(path[i], path[i + 1], -capacity);
+        changeEdge(path[i + 1], path[i], capacity);
+    }
+}
+
+void maxFlow :: changeEdge(int a, int b, int w){
+    for(auto it = adj[a].begin(); it != adj[a].end(); ++it){
+        if((*it).first == b) {
+            ((*it).second) += w;
+            if((*it).second == 0){
+                adj[a].erase(it);
+            }
+            return;
+        }
+    }
+    adj[a].push_back(make_pair(b, w));
+}
+
+int maxFlow :: solve(){
+    vector<int> path = {0};
+    while(!path.empty()){
+        path = shortestPath();
+        push(path);
+    }
+    return maxflow;
+}
+
+int32_t main(){
+    // s-3>a-2>t;
+    int n;
+    cin >> n;
+    vector<vector<pair<int,int>>> adj(n);
+    int m;
+    cin >> m;
+    int l, r, c;
+    for(int i = 0; i < m; i++){
+        cin >> l >> r >> c;
+        adj[l].push_back(make_pair(r, c));
+    }
+    maxFlow mf = new maxFlow(n, adj, 0, n - 1);
+    cout << "maxflow = "<< mf->solve() << endl;
+}
